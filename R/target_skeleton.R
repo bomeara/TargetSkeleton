263ebdb0e9f4690cda09df1edd940d6e5938edde
Brian O'Meara <omeara.brian@gmail.com>
2026-02-21 22:22:15 -0500

    adjusting spacing

--- R/target_skeleton.R
+++ R/target_skeleton.R
@@ -10,28 +10,30 @@ target_skeleton <- function(scratch_path="", include_examples=TRUE) {
 	dir.create("results")
 	dir.create("data")
 	cat(
-		'source("_targets.R")
-		tar_make()
-		tar_load(everything(), strict=FALSE) # If you are creating something with large objects where you do not want everything in memory, comment this out',
+		'
+source("_targets.R")
+tar_make()
+tar_load(everything(), strict=FALSE) # If you are creating something with large objects where you do not want everything in memory, comment this out',
 		file="run.R"
 	)
 	cat(
-		'library(targets)
-		library(tarchetypes)
-		library(crew)
-		ncores_to_allocate <- 2
-		tar_option_set(
-			packages = c(
-					"dplyr",
-					"phylogram",
-					"ggplot2",
-					"cowplot",
-					"stats"
-			),
-        	controller = crew_controller_local(workers = ncores_to_allocate) 
-		)
+		'
+library(targets)
+library(tarchetypes)
+library(crew)
+ncores_to_allocate <- 2
+tar_option_set(
+	packages = c(
+		"dplyr",
+		"phylogram",
+		"ggplot2",
+		"cowplot",
+		"stats"
+	),
+	controller = crew_controller_local(workers = ncores_to_allocate) 
+)
 		
-		source("R/functions.R")
+source("R/functions.R")
 		
 		',
 		file = "_targets.R"
@@ -39,32 +41,32 @@ target_skeleton <- function(scratch_path="", include_examples=TRUE) {
 	
 	if(include_examples) {
 		cat(
-			'list(
-				tar_target(input_file, command = "data/input.csv", format="file"),
-				tar_target(loaded_file, read_file(input_file)),
-				tar_target(means, c(0, 1, 10)),
-				tar_target(sds, c(0.2, 0.5)),
-				tar_target(reps, 100)),
-				tar_target(
-					name = sim_result,
-					command = doRun(
-						means,
-						sds,
-						reps
-					),
-					pattern = cross(
-						means,
-						sds,
-						reps
-					)
-				)
-			)',
+'list(
+	tar_target(input_file, command = "data/input.csv", format="file"),
+	tar_target(loaded_file, read_file(input_file)),
+	tar_target(means, c(0, 1, 10)),
+	tar_target(sds, c(0.2, 0.5)),
+	tar_target(reps, 100)),
+	tar_target(
+		name = sim_result,
+		command = doRun(
+			means,
+			sds,
+			reps
+		),
+		pattern = cross(
+			means,
+			sds,
+			reps
+		)
+	)
+)',
 			file = "_targets.R",
 			append = TRUE
 		)		
 	} else {
 		cat(
-			'list()
+'list()
 			',
 			file = "_targets.R",
 			append=TRUE
@@ -73,22 +75,22 @@ target_skeleton <- function(scratch_path="", include_examples=TRUE) {
 	
 	if(include_examples) {
 		fake_data <- data.frame(letter = LETTERS[1:10], number = c(1:9, -99), bool = rep(c(TRUE, FALSE),5))
-		write.csv(fake_data, file="data/input.csv")
+		write.csv(fake_data, file="data/input.csv", row.names=FALSE)
 	}
 	
 	if(include_examples) {
 		cat('
-		 read_file <- function(input_name) {
-		 	result <- read.csv(input_name)
-			result_desired <- result[result$number>0,] # as an example of filtering
-			return(result_desired)
-		 }
-		 
-		  doRun <- function(m, s, r) {
-		  	samples <- stats::rnorm(n = r, mean = m, sd = s)
-			result <- data.frame(mean = m, sd = s, nrep = r, lowest = min(samples), highest = max(samples))
-			return(result)
-		  }
+read_file <- function(input_name) {
+	result <- read.csv(input_name)
+	result_desired <- result[result$number>0,] # as an example of filtering
+	return(result_desired)
+}
+
+doRun <- function(m, s, r) {
+	samples <- stats::rnorm(n = r, mean = m, sd = s)
+	result <- data.frame(mean = m, sd = s, nrep = r, lowest = min(samples), highest = max(samples))
+	return(result)
+}
 		',
 			file = "R/functions.R"
 		)
